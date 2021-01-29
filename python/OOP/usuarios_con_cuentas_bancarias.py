a68111ea560ec5435aadb2d3aadf73970ed94e47
Juan Alvarez <juan.alvarez1989@gmail.com>
2021-01-29 00:23:37 -0300

    Python OOP: usuarios_con_cuentas_bancarias updated

--- python/OOP/usuarios_con_cuentas_bancarias.py
+++ python/OOP/usuarios_con_cuentas_bancarias.py
@@ -1,5 +1,12 @@
+class User:
+    def __init__(self, name, email):
+        self.email = email
+        self.name = name
+        self.account = BankAccount(name=name, int_rate=0.02, balance=0)
+
 class BankAccount:
-    def __init__(self, int_rate=0.02, balance=0):
+    def __init__(self, name, int_rate, balance):
+        self.username = name
         self.int_rate = int_rate
         self.balance = balance
 
@@ -11,67 +18,41 @@ class BankAccount:
         if self.balance >= amount:
             self.balance -= amount
         else:
-            print("Insufficient balance")
+            print("Insufficient funds!")
             self.balance -= 5
         return self
 
-    def display_account_info(self):
-        #print("BankAccount:",",", "Balance: $", self.balance)
-        print("User: ", ", ", "Balance: $", self.balance) #from user
-#como puedo imprimir el nombre de usuario
-        return self
-
     def yield_interest(self):
-        if self.balance>0:
+        if self.balance > 0:
             self.balance += self.balance * self.int_rate
         else:
             self.balance = self.balance
         return self
 
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.account = BankAccount()
-
-    def make_withdrawal (self, amount):
-        self.account.withdraw(amount)
-        return self
-
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
-        return self
-
-    def display_user_balance (self):
-        self.account.display_account_info()
+    def display_account_info(self):
+        print("User:", self.username, ",", "Balance: $", self.balance)
+        return self  # account_number* to be added if we include several bank accounts
 
     def transfer_money (self, other_user, amount):
-        self.account.withdraw(amount)
-        other_user.account.deposit(amount)
+        if self.balance >= amount:
+            self.balance -= amount
+            other_user.balance +=amount
+            print("This transaction was successfully completed.")
+        else:
+            print("Rejected transaction! Insufficient funds.")
         return self
 
-#print nombre usuario / cuenta
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+michael = User("Michael Choi", "michael@python.com")
 
-#Permite al usuario tener varias cuentas; actualiza los metodos para que el usuario pueda especificar
-# de cual cuenta ellos quieren depositar o retirar
+#accounts info: balance = 0
+guido.account.display_account_info()
+monty.account.display_account_info()
+michael.account.display_account_info()
 
-guido = User("Guido van Rossum")  # instancia de objeto
-monty = User("Monty Python")
-michael = User("Michael Choi")
-
-guido.display_user_balance()
-monty.display_user_balance()
-michael.display_user_balance()
-guido.make_withdrawal(100)
-guido.display_user_balance()
-guido.make_deposit(100)
-guido.display_user_balance()
-monty.make_deposit(11)
-monty.display_user_balance()
-michael.make_deposit(70)
-michael.make_withdrawal(35)
-michael.display_user_balance()
-print("*"*80)
-
-
-
-
+guido.account.deposit(100).withdraw(50).yield_interest().display_account_info()
+#guido's current balance = 51.
+guido.account.transfer_money(monty.account,60).display_account_info() #rejected, not enough money.
+guido.account.transfer_money(monty.account,50).display_account_info() #Approved, $50 added to Monty's account
+monty.account.display_account_info()
