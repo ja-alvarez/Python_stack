d91ca5186946fac95ec8192219ef533b1b7978b8
Juan Alvarez <juan.alvarez1989@gmail.com>
2021-02-03 08:55:17 -0300

    Django: primer proyecto and time_display modified

--- django/django_intro/mi_primer_proyecto/Mi_App/urls.py
+++ django/django_intro/mi_primer_proyecto/Mi_App/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('blogs/create', views.create),
     path('blogs/<int:_number>', views.show),
     path('blogs/<int:_number>/edit', views.edit),
-    path('blogs/<int:_number>/delete', views.destroy), #hacer que _number solo acepte INT
+    path('blogs/<int:_number>/delete', views.destroy),
 #    path('blogs/json', views.json)
 ]
